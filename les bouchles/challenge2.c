a75cbe45f34d743fb3913384d7abb265ac31da65
IlyasBh02 <ilyasbahsi9@gmail.com>
2024-09-26 11:18:56 +0100

    continuation

--- les bouchles/challenge2.c
+++ les bouchles/challenge2.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-  
-  int N, F, i ;
-  printf("veuillez entrer la valleur de N : ") ;
-  scanf("%d", &N) ;
-  F = 1 ;
-  if (N>0){
-  for (i=2;i<=N;i++){
-      F = F*i ;
-  }
-      printf("la factorielle de %d est : %.d",N,F) ;
-  }
-  else if (N==0)
-    printf("la factorielle est : 1") ;
     
-    else 
-    printf("Veuillez entrer un nombre positif") ;
+    int num, i, F ;
+    
+    printf("Veuillez entrer le nombre :");
+    scanf("%d", &num);
+    
+    F = 1 ;
+    
+      for (i=1 ; i<=num ;i++) {
+      F=F*i;
+      }
+      printf("Le factorielle de %d est : %d ",num,F);
+      
+
+    
     return 0;
 }
